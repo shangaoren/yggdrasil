99dcf5345212085ce1a72ad6c98b43068f3e6860
Florian GERARD <fgerard@beeionic.com>
2023-01-08 20:40:21 +0100

    updated assertion

--- framework/Assertion.hpp
+++ framework/Assertion.hpp
@@ -1,11 +1,12 @@
 #pragma once
 namespace kernel
 {
-	static void stop()
-	{
-		asm volatile("bkpt #0");
-	}
+
 #ifdef KDEBUG
+static void stop()
+{
+	asm volatile("bkpt #0");
+}
 	#define Y_ASSERT(cond) ((cond) ? (void)0U : kernel::stop())
 #else
 	#define Y_ASSERT(cond) ((void)0)
